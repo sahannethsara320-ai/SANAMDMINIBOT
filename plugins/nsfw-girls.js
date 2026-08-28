1ffabbcd3e471565147772b0f520c9cf755d6c07
sahannethsara320-ai <sahannethsara320@gmail.com>
2026-08-28 19:00:38 +0530

    Update nsfw-girls.js

--- plugins/nsfw-girls.js
+++ plugins/nsfw-girls.js
@@ -2,35 +2,11 @@ const { cmd } = require("../arslan");
 const { fakevCard } = require('../lib/fakevCard');
 
 cmd({
-  pattern: "boobs",
-  alias: ["xboobs", "bobs"],
-  desc: "NSFW Image Generator",
-  category: "adult",
-  react: "🔞",
-  filename: __filename
-}, async (conn, mek, m, { from, reply }) => {
-  try {
-    const apiUrl = "https://api.azbry.com/api/ai/imagegen?prompt=Nude+Fuck+Real+Girl";
-
-    await conn.sendMessage(from, {
-      image: { url: apiUrl },
-      caption: "🔞 *NSFW Image*\n\n© SANA MD"
-    }, {
-      quoted: fakevCard
-    });
-
-  } catch (err) {
-    console.log(err);
-    reply("❌ Image එක ලබාදෙන්න බැරි වුණා. Please try again later.");
-  }
-});
-
-cmd({
   pattern: "xgirl",
   alias: ["xgirls", "ximg"],
-  desc: "NSFW Image Generator",
-  category: "adult",
-  react: "🔞",
+  desc: "Random Anime Girl Image",
+  category: "fun",
+  react: "🌸",
   filename: __filename
 }, async (conn, mek, m, { from, reply }) => {
   try {
@@ -38,7 +14,7 @@ cmd({
 
     await conn.sendMessage(from, {
       image: { url: apiUrl },
-      caption: "🔞 *NSFW Image*\n\n© SANA MD"
+      caption: "🌸 *Random X Girl*\n\n© SANA MD"
     }, {
       quoted: fakevCard
     });
